241a6e74bda7d7fb61b1231385eab103b0cea18f
Archimedes Trajano <developer@trajano.net>
2025-04-05 16:46:57 -0400

    style fixes

--- di/basic_container.py
+++ di/basic_container.py
@@ -1,23 +1,23 @@
 import dataclasses
 import inspect
-import typing
+from typing import TypeVar, Type, Set, Generic, Dict, List, Self, get_type_hints, Any
 
 from . import CycleDetectedError
 from .container import Container
 from .exceptions import ContainerError, ComponentNotFoundError
 
-T = typing.TypeVar("T")
+T = TypeVar("T")
 
 
 @dataclasses.dataclass
-class ImplementationDefinition(typing.Generic[T]):
-    type: typing.Type[T]
+class ImplementationDefinition(Generic[T]):
+    type: Type[T]
     """The primary type (class) of the implementation."""
 
-    satisfied_types: typing.Set[typing.Type[typing.Any]]
+    satisfied_types: Set[Type[Any]]
     """A set of types satisfied by the implementation (excluding 'object')."""
 
-    dependencies: typing.Set[typing.Type[typing.Any]]
+    dependencies: Set[Type[Any]]
     """A set of types that are constructor dependencies of the implementation."""
 
     implementation: T | None
@@ -26,12 +26,12 @@ class ImplementationDefinition(typing.Generic[T]):
 
 class BasicContainer(Container):
     def __init__(self):
-        self._definitions: list[ImplementationDefinition] = []
-        self._type_map: dict[type, typing.Any] = {}
-        self._instances: set[typing.Any] = set()
+        self._definitions: list[ImplementationDefinition[Any]] = []
+        self._type_map: dict[Type, Any] = {}
+        self._instances: set[Any] = set()
         self._locked: bool = False
 
-    def add_component_type(self, component_type: typing.Type[T]) -> typing.Self:
+    def add_component_type(self, component_type: Type[T]) -> Self:
         if self._locked:
             raise ContainerError("Container is locked after first resolution.")
 
@@ -61,7 +61,7 @@ class BasicContainer(Container):
         return self
 
 
-    def get_component(self, component_type: typing.Type[T]) -> T:
+    def get_component(self, component_type: Type[T]) -> T:
         """
         Gets a single component from the container that satisfies the given type.
         This resolves all constructor dependencies for the component.
@@ -75,23 +75,23 @@ class BasicContainer(Container):
         else:
             return maybe_component
 
-    def get_components(self, component_type: typing.Type[T]) -> typing.List[T]:
+    def get_components(self, component_type: Type[T]) -> List[T]:
         if not self._locked:
             self._resolve_all()
         return [impl for impl in self._instances if isinstance(impl, component_type)]
 
-    def get_optional_component(self, component_type: typing.Type[T]) -> T | None:
+    def get_optional_component(self, component_type: Type[T]) -> T | None:
         if not self._locked:
             self._resolve_all()
         return self._type_map.get(component_type)
 
     def _resolve_all(self):
         self._locked = True
-        resolving: set[typing.Type] = set()
+        resolving: set[Type] = set()
 
-        type_to_definition : typing.Dict[typing.Type, ImplementationDefinition]= {d.type: d for d in self._definitions}
+        type_to_definition : Dict[Type, ImplementationDefinition]= {d.type: d for d in self._definitions}
 
-        def resolve(component_type: typing.Type[T]) -> T:
+        def resolve(component_type: Type[T]) -> T:
             if component_type in self._type_map:
                 return self._type_map[component_type]
 
@@ -104,7 +104,7 @@ class BasicContainer(Container):
             resolving.add(component_type)
 
             resolved_definition = type_to_definition[component_type]
-            init_hints = typing.get_type_hints(resolved_definition.type.__init__)
+            init_hints = get_type_hints(resolved_definition.type.__init__)
             kwargs = {
                 param: resolve(dep_type)
                 for param, dep_type in init_hints.items()
@@ -126,10 +126,10 @@ class BasicContainer(Container):
     def __len__(self) -> int:
         return len(self._definitions)
 
-    def __getitem__(self, component_type: typing.Type[T]) -> T:
+    def __getitem__(self, component_type: Type[T]) -> T:
         return self.get_component(component_type)
 
-    def __contains__(self, component_type: typing.Type[typing.Any]) -> bool:
+    def __contains__(self, component_type: Type[Any]) -> bool:
         if not self._locked:
             self._resolve_all()
         return component_type in self._type_map
