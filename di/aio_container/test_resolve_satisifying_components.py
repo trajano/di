3f525f63241f21fcc3a84d011b404266bad2a853
Archimedes Trajano <developer@trajano.net>
2025-04-08 13:08:29 -0400

    resolver updated

--- di/aio_container/test_resolve_satisifying_components.py
+++ di/aio_container/test_resolve_satisifying_components.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from di.enums import ComponentScope
 from ._types import ComponentDefinition, ResolvedComponent
-from .resolver import resolve_satisfying_components
+from .resolver import resolve_satisfying_components, resolve_container_scoped_only
 from ._convert_to_factory import convert_to_factory
 
 
@@ -12,8 +12,8 @@ class C:
 
 
 class B:
-    def __init__(self, *, c: C):
-        self.c = c
+    def __init__(self, *, stuff: C):
+        self.c = stuff
 
 
 class A:
@@ -21,19 +21,19 @@ class A:
         self.b = b
 
 
-def make_definition(cls, deps: set[type]):
+def make_definition(cls, deps: set[type], scope: ComponentScope = ComponentScope.FUNCTION):
     return ComponentDefinition(
         type=cls,
         satisfied_types={cls},
         dependencies=deps,
         collection_dependencies=set(),
         factory=convert_to_factory(cls),
-        scope=ComponentScope.FUNCTION
+        scope=scope
     )
 
 
 @pytest.mark.asyncio
-async def test_resolve_linear_chain():
+async def test_resolve_linear_chain_with_function_scope_only():
     defs = [
         make_definition(C, deps=set()),
         make_definition(B, deps={C}),
@@ -46,3 +46,27 @@ async def test_resolve_linear_chain():
     assert isinstance(a, A)
     assert isinstance(a.b, B)
     assert isinstance(a.b.c, C)
+
+
+@pytest.mark.asyncio
+async def test_resolve_with_container_scoped_component():
+    defs = [
+        make_definition(C, deps=set(), scope=ComponentScope.CONTAINER),
+        make_definition(B, deps={C}),
+        make_definition(A, deps={B}),
+    ]
+
+    resolved = await resolve_container_scoped_only(defs)
+
+    result = await resolve_satisfying_components(A, resolved_components=resolved, definitions=defs)
+    assert len(result) == 1
+    a = result[0]
+    assert isinstance(a, A)
+    assert isinstance(a.b, B)
+    assert isinstance(a.b.c, C)
+
+
+@pytest.mark.asyncio
+async def test_resolve_empty_inputs():
+    result = await resolve_satisfying_components(object, resolved_components=[], definitions=[])
+    assert result == []
