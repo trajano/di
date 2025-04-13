0ff2ff8e5f4e3a05f52b2c7b182a4874d30cc272
Archimedes Trajano <developer@trajano.net>
2025-04-13 15:01:51 -0400

    refactor: expose ComponentScope

--- di_aio/__init__.py
+++ di_aio/__init__.py
@@ -6,10 +6,14 @@ utilizes a single default configurable container and runtime context.
 
 from .decorators import autowired, component, factory
 from .default_container import default_container
+from .enums import ComponentScope
+from .exceptions import ContainerError
 from .protocols import ConfigurableContainer, Context
 
 __all__ = [
+    "ComponentScope",
     "ConfigurableContainer",
+    "ContainerError",
     "Context",
     "autowired",
     "component",
--- di_aio/alt.py
+++ di_aio/alt.py
@@ -6,10 +6,14 @@ containers.
 from .configurable_container import ConfigurableAioContainer
 from .context import AioContext
 from .decorators import autowired, autowired_with_container, component, factory
+from .enums import ComponentScope
+from .exceptions import ContainerError
 
 __all__ = [
     "AioContext",
+    "ComponentScope",
     "ConfigurableAioContainer",
+    "ContainerError",
     "autowired",
     "autowired_with_container",
     "component",
