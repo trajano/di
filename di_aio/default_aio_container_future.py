42062e36f9fc9e4f3ed232ed3a297ed8d2960fbd
Archimedes Trajano <developer@trajano.net>
2025-04-14 02:07:38 -0400

    relocate

--- di_aio/default_aio_container_future.py
+++ di_aio/default_aio_container_future.py
@@ -3,13 +3,3 @@
 from .future_context import FutureContext
 
 default_context_holder = FutureContext()
-
-
-def reset_default_aio_context() -> None:
-    """Reset the default AIO context.
-
-    This should only be called for testing purposes.
-
-    :raises ContainerError: If reset is called improperly (internally).
-    """
-    default_context_holder.reset()
--- di_aio/testing.py
+++ di_aio/testing.py
@@ -1,6 +1,19 @@
 """Testing support."""
 
 from .decorators import autowired_with_context
-from .default_aio_container_future import reset_default_aio_context
+from .default_container import default_container
+from .default_aio_container_future import default_context_holder
+
+
+def reset_default_aio_context() -> None:
+    """Reset the default AIO context.
+
+    This should only be called for testing purposes.
+
+    :raises ContainerError: If reset is called improperly (internally).
+    """
+    default_container.clear()
+    default_context_holder.reset()
+
 
 __all__ = ["autowired_with_context", "reset_default_aio_context"]
